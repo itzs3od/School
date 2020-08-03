109ebfb0aa1b57421fae8f0e3909c9bf287cfac7
itzs3odx <is3od.is@gmail.com>
2020-08-03 06:55:26 +0300

    fourth commit

---
+++ Pages/School/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using School.Model;
+
+namespace School.Pages.School
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+        public async Task OnGet(int id)
+        {
+            Student = await _db.Student.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var StudentFromDB = await _db.Student.FindAsync(Student.Id);
+                StudentFromDB.FirstName = Student.FirstName;
+                StudentFromDB.LastName = Student.LastName;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+
+    }
+}
---
+++ Pages/School/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using School.Model;
+
+namespace School.Pages.School
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Student> Students { get; set; }
+        public async Task OnGet()
+        {
+            Students = await _db.Student.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var Student = await _db.Student.FindAsync(id);
+            if(Student == null)
+            {
+                return NotFound();
+            }
+            _db.Student.Remove(Student);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
